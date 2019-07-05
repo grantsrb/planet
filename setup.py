4a65c2f1974ad4250aed499fa80db3f7e494485f
Satchel Grant <grantsrb@gmail.com>
2019-07-04 17:15:24 -0700

    add license and setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import planet
+
+setup(name='planet',
+      version=planet.__version__,
+      description='Implementation of PlaNet',
+      author='Satchel Grant',
+      author_email='grantsrb@gmail.com',
+      url='https://github.com/grantsrb/planet.git',
+      install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+      long_description='''
+          The planet package contains methods for implementing the algorithms in the
+          paper: Learning Latent Dynamics from Pixels for Planning.
+          ''',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: MacOS :: MacOS X :: Ubuntu',
+          'Topic :: Scientific/Engineering :: Information Analysis'],
+      packages=find_packages(),
+      )
